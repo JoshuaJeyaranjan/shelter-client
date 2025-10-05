8d2bc39a56d80433c5e1df86c5d1632253362ad2
JoshuaJeyaranjan <141107926+JoshuaJeyaranjan@users.noreply.github.com>
2025-10-05 19:18:11 -0400

    map page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ShelterList from "./components/ShelterList/ShelterList";
 import HomePage from "./pages/HomePage/HomePage";
-
+import SheltersMapPage from "./pages/SheltersMapPage/SheltersMapPage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shelters" element={<ShelterList />} />
-        {/* Optional: redirect unknown paths */}
+        <Route path='/shelter-map' element={<SheltersMapPage/>} />
+        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
